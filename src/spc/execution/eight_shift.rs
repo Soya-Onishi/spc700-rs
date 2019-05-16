91d6533c2c35f219f48b73bfd161611c0997a71f
Soya Ohnishi <ohnishi_soya@s.okayama-u.ac.jp>
2019-05-16 22:47:34 +0900

    Add new file to execute shift

---
+++ src/spc/execution/eight_shift.rs
@@ -0,0 +1,57 @@
+pub fn asl(op0: u8, carry_flag: bool) -> (u8, u8) {
+    let shifter = |op, carry| -> u8 {
+        op << 1;
+    };
+    let is_carry = |op| -> bool {
+        op & 0x80 > 0
+    };
+
+    shift(op0, carry_flag, shifter, is_carry)
+}
+
+pub fn rol(op0: u8, carry_flag: bool) -> (u8, u8) {
+    let shifter = |op, carry| -> u8 {
+        let c = if carry_flag { 1 } else { 0 };
+        op0 << 1  | c
+    };
+    let is_carry = |op| -> bool {
+        op & 0x80 > 0
+    };
+
+    shift(op0, carry_flag, shifter, is_carry)
+}
+
+pub fn lsr(op0: u8, carry_flag: bool) -> (u8, u8) {
+    let shifter = |op, carry| {
+        (op >> 1) & 0x7f
+    };
+    let is_carry = |op| -> bool {
+        op & 1 > 0
+    };
+
+    shift(op0, carry_flag, shifter, is_carry)
+}
+
+pub fn ror(op0: u8, carry_flag: bool) -> (u8, u8) {
+    let shifter = |op, carry| -> u8 {
+        let c = if carry_flag { 0x80 } else { 0 };
+        op0 >> 1 | c
+    };
+    let is_carry = |op| -> bool {
+        op & 1 > 0
+    };
+
+    shift(op0, carry_flag, shifter, is_carry)
+}
+
+fn shift(op0: u8, carry_flag: bool, shifter: impl Fn(u8, bool) -> u8, is_carry: impl Fn(u8) -> bool) -> (u8, u8) {
+    let res = shifter(op0, carry_flag);
+
+    let mask = 0b1000_0011;
+    let carry = is_carry(op0) as u8;
+    let sign = res & 0x80;
+    let zero = ((res == 0) as u8) << 1;
+    let flag = mask & (carry | sign | zero);
+
+    (res, flag)
+}
