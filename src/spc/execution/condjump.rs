c38954b91301d0d1ca4a153903a49b9e4b6ce2cf
Soya Ohnishi <ohnishi_soya@s.okayama-u.ac.jp>
2019-05-18 21:32:08 +0900

    Add new file to execute condition jump

---
+++ src/spc/execution/condjump.rs
@@ -0,0 +1,70 @@
+
+type RetType = (u16, bool); // (destination, is_branched)
+
+pub fn bpl(pwd: u8, pc: u16, offset: u8) -> RetType {
+    branch(!pwd, 0x80, pc, offset)
+}
+
+pub fn bmi(pwd: u8, pc: u16, offset: u8) -> RetType {
+    branch(pwd, 0x80, pc, offset)
+}
+
+pub fn bvc(pwd: u8, pc: u16, offset: u8) -> RetType {
+    branch(!pwd, 0x40, pc, offset)
+}
+
+pub fn bvs(pwd: u8, pc: u16, offset: u8) -> RetType {
+    branch(pwd, 0x40, pc, offset)
+}
+
+pub fn bcc(pwd: u8, pc: u16, offset: u8) -> RetType {
+    branch(!pwd, 0x01, pc, offset)
+}
+
+pub fn bcs(pwd: u8, pc: u16, offset: u8) -> RetType {
+    branch(pwd, 0x01, pc, offset)
+}
+
+pub fn bne(pwd: u8, pc: u16, offset: u8) -> RetType {
+    branch(!pwd, 0x02, pc, offset)
+}
+
+pub fn beq(pwd: u8, pc: u16, offset: u8) -> RetType {
+    branch(pwd, 0x02, pc, offset)
+}
+
+pub fn bbs(byte: u8, pc: u16, offset: u8) -> RetType {
+    branch(byte, 0x01, pc, offset)
+}
+
+pub fn bbc(byte: u8, pc: u16, offset: u8) -> RetType {
+    branch(!byte, 0x01, pc, offset)
+}
+
+pub fn cbne(byte: u8, acc: u8, pc: u16, offset: u8) -> RetType {
+    let is_branch = byte != acc;
+    let pc = pc as i32;
+    let offset = if is_branch { offset as i32 } else { 4 };
+
+    ((pc + offset) as u16, is_branch)
+}
+
+pub fn dbnz(byte: u8, pc: u16, offset: u8) -> (u8, RetType) {
+    let byte = byte.wrapping_sub(1);
+
+    let is_branch = byte != 0;
+    let pc = pc as i32;
+    let offset = if is_branch { offset as i32 } else { 4 };
+
+    (byte, ((pc + offset) as u16, is_branch))
+}
+
+fn branch(byte: u8, bit_offset: u8, pc: u16, offset: u8) -> (u16, bool) {
+    let branch = byte & bit_offset > 0;
+
+
+    let pc = pc as i32;
+    let offset = if branch { offset as i32 } else { 4 };
+
+    (pc.wrapping_add(offset) as u16, branch)
+}
