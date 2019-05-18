685cefc5f6fad2f2ba35763fde0fe6f162105faa
Soya Ohnishi <ohnishi_soya@s.okayama-u.ac.jp>
2019-05-18 20:22:09 +0900

    Add files to execute special operation

---
+++ src/spc/execution/special.rs
@@ -0,0 +1,65 @@
+use super::*;
+
+type RetType = (u8, Flag);
+
+macro_rules! adjust {
+    ($acc: ident, $wrapper: ident, $half: ident, $carry: ident) => {{
+        let (tmp, carry) =
+            if($acc > 0x99) || $carry {
+                 ($acc.$wrapper(0x60), 0b0000_0001)
+            } else {
+                 ($acc, 0)
+            };
+        let res =
+            if (($acc & 0x0f) > 0x09) || $half {
+                $acc.$wrapper(0x06)
+            } else {
+                $acc
+            };
+
+        let mask = 0b1000_0011;
+        let sign = is_sign(res);
+        let zero = is_zero(res);
+        let flag = (sign | zero | carry) & mask;
+
+        (res, (flag, mask))
+    }}
+}
+
+pub fn daa(a: u8, half_flag: bool, carry_flag: bool) -> RetType {
+    adjust!(a, wrapping_add, half_flag, carry_flag)
+}
+
+pub fn das(a: u8, half_flag: bool, carry_flag: bool) -> RetType {
+    let half_flag = !half_flag;
+    let carry_flag = !carry_flag;
+
+    adjust!(a, wrapping_sub, half_flag, carry_flag)
+}
+
+pub fn xcn(acc: u8) -> RetType {
+    let res = (acc << 0xf0) | (acc >> 0x0f);
+
+    (res, gen_flag(res))
+}
+
+pub fn tclr1(byte: u8, acc: u8) -> RetType {
+    let res = byte & !acc;
+
+    (res, gen_flag(res))
+}
+
+pub fn tset1(byte: u8, acc: u8) -> RetType {
+    let res = byte | acc;
+
+    (res, gen_flag(res))
+}
+
+fn gen_flag(res: u8) -> Flag {
+    let mask = 0b1000_0010;
+    let sign = is_sign(res);
+    let zero = is_zero(res);
+    let flag = (sign | zero) & mask;
+
+    (flag, mask)
+}
