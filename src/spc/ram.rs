d4ce47d14496e42c61aa3e70802e4263d755a206
Soya Ohnishi <ohnishi_soya@s.okayama-u.ac.jp>
2019-05-27 22:38:04 +0900

    Implement 64KB Ram

--- src/spc/ram.rs
+++ src/spc/ram.rs
@@ -1,55 +1,33 @@
 use super::core::Spc700;
-
-/*
-pub struct Ram<'a> {
-    core: Option<&'a Spc700<'a>>,
-    ram: u8,
-}
-*/
-/*
-pub trait RamManipulate<T> {
-    fn read_ram(&self, addr: T) -> u8;
-    fn write_ram(&mut self, addr: T, data: u8);
-}
-
-impl RamManipulate<u8> for Spc700 {
-    fn read_ram(&self, addr: u8) -> u8 {
-        self.ram
-    }
-
-    fn write_ram(&mut self, addr: u8, data: u8) {
-
-    }
-}
-
-impl RamManipulate<u16> for Spc700 {
-    fn read_ram(&self, addr: u16) -> u8 {
-        self.ram
-    }
-
-    fn write_ram(&mut self, addr: u16, data: u8) {
-
-    }
-}
-*/
+use std::fs;
 
 pub struct Ram {
-    ram: u8
+    ram: [u8; 0x10000],
 }
 
 impl Ram {
     pub fn new() -> Ram {
         Ram {
-            ram: 0
+            ram: [0; 0x10000],
+        }
+    }
+
+    pub fn load(&mut self, filename: String, start_pos: u16, set_pos: u16) {
+        let binaries = fs::read(filename).expect("not found");
+        let start_pos = start_pos as usize;
+        let set_pos = set_pos as usize;
+
+        for (offset, bin) in binaries[start_pos..].iter().enumerate() {
+            self.ram[set_pos + offset] = bin.clone();
         }
     }
 
     pub fn read(&self, addr: u16) -> u8 {
-        0
+        self.ram[addr as usize]
     }
 
-    pub fn write(&self, addr: u16, data: u8) {
-
+    pub fn write(&mut self, addr: u16, data: u8) {
+        self.ram[addr as usize] = data;
     }
 }
 
