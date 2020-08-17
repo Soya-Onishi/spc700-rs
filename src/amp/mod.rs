7a180f0de0e1d40dc289af45cbf72cd3dce6bb63
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-08-18 05:09:37 +0900

    remove unused variables and codes

--- src/amp/mod.rs
+++ src/amp/mod.rs
@@ -5,7 +5,6 @@ use crate::emulator::core::Spc700;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use std::collections::VecDeque;
 use std::f32;
-use std::f64;
 use std::sync;
 use std::thread;
 
@@ -82,10 +81,6 @@ impl Amplifier {
   }
 }
 
-fn only_dec(x: f64) -> f64 {  
-  x - (x as i32) as f64
-}
-
 struct RingBuffer<T: Clone + Copy> {
   buf_size: usize,
   pub vector: Vec<(T, T)>,
@@ -152,8 +147,7 @@ fn build_stream<T: cpal::Sample>(
   device: &cpal::Device,
   config: &cpal::StreamConfig,
   buffer: sync::Arc<sync::Mutex<RingBuffer<i16>>>,
-) -> cpal::Stream {
-  let sampling_rate = config.sample_rate.0 as u32;
+) -> cpal::Stream {  
   let channels = config.channels as usize;
   let err_fn = |err| eprintln!("an error occurred on stream: {}", err);  
 
@@ -179,7 +173,7 @@ fn build_stream<T: cpal::Sample>(
           buf.read(require_length)
         };
 
-        assign_output(data, read_data, channels, sampling_rate);        
+        assign_output(data, read_data, channels);        
       },
       err_fn,
     )
@@ -187,51 +181,16 @@ fn build_stream<T: cpal::Sample>(
   stream
 }
 
-fn assign_output<T: cpal::Sample>(data: &mut [T], output: Vec<(i16, i16)>, channels: usize, sampling_rate: u32) -> () {
-  static mut LEFT_IN: [f32; 2] = [0.0; 2];
-  static mut RIGHT_IN: [f32; 2] = [0.0; 2];
-  static mut LEFT_OUT: [f32; 2] = [0.0; 2];
-  static mut RIGHT_OUT: [f32; 2] = [0.0; 2];  
-
-  let left_in = [0.0; 2];
-  let right_in = [0.0; 2];
-  let left_out = [0.0; 2];
-  let right_out = [0.0; 2];
-
-  let q = 1.0 / f32::sqrt(2.0);
-  let freq = sampling_rate as f32 / 2.0;
-  let omega = std::f32::consts::PI;
-  let alpha = 2.0 * q;
-
-  let a0 =  1.0 + alpha;
-  let a1 =  0.0; //-2.0 * f32::cos(omega);
-  let a2 =  1.0 - alpha;
-  let b0 =  0.5; //(1.0 - f32::cos(omega)) / 2.0;
-  let b1 =  1.0; // 1.0 - f32::cos(omega);
-  let b2 =  0.5; // (1.0 - f32::cos(omega)) / 2.0;
-
-  let a = [a0, a1, a2];
-  let b = [b0, b1, b2];
-
+fn assign_output<T: cpal::Sample>(data: &mut [T], output: Vec<(i16, i16)>, channels: usize) -> () {
   if channels == 1 {
     let outputs = output.iter().map(|(left, right)| (left + right) / 2);
     for (sample, output) in data.iter_mut().zip(outputs) {
       *sample = cpal::Sample::from(&output);
     }    
   } else {
-    let each_len = channels / 2;
-    let lefts: Vec<i16> = output.iter().map(|&(left, _)| left).collect();
-    let rights: Vec<i16> = output.iter().map(|&(_, right)| right).collect();
-    // let (lefts, left_in, left_out) = low_pass_filter(lefts, left_in, left_out, a, b);
-    // let (rights, right_in, right_out) = low_pass_filter(rights, right_in, right_out, a, b);
-    let left: Vec<T> = lefts.iter().map(|left| cpal::Sample::from(left)).collect();
-    let right: Vec<T> = rights.iter().map(|right| cpal::Sample::from(right)).collect();
-    unsafe {
-      LEFT_IN   = left_in;
-      RIGHT_IN  = right_in;
-      LEFT_OUT  = left_out;
-      RIGHT_OUT = right_out;
-    }
+    let each_len = channels / 2;    
+    let left: Vec<T> = output.iter().map(|(left, _)| cpal::Sample::from(left)).collect();
+    let right: Vec<T> = output.iter().map(|(_, right)| cpal::Sample::from(right)).collect();    
 
     for (frame, idx) in data.chunks_mut(channels).zip(0..) {      
       for (sample, ch) in frame.iter_mut().zip(0..) {
@@ -239,67 +198,5 @@ fn assign_output<T: cpal::Sample>(data: &mut [T], output: Vec<(i16, i16)>, chann
         else             { *sample = right[idx] }        
       }
     }
-  }
-}
-
-fn low_pass_filter(inputs: Vec<f32>, last_in: [f32; 2], last_out: [f32; 2], a: [f32; 3], b: [f32; 3]) -> (Vec<f32>, [f32; 2], [f32; 2]) {  
-  let mut last_in = last_in;
-  let mut last_out = last_out;
-
-  let outputs = inputs.iter().map(|&input| {
-    let i0 = (b[0] / a[0]) * input;
-    let i1 = (b[1] / a[0]) * last_in[0];
-    let i2 = (b[2] / a[0]) * last_in[1];
-    let o0 = (a[1] / a[0]) * last_out[0];
-    let o1 = (a[2] / a[0]) * last_out[1];
-
-    let output = i0 + i1 + i2 - o0 - o1;
-    last_in = [input, last_in[0]];
-    last_out = [output, last_out[0]];
-
-    output
-  });
-
-  (outputs.collect(), last_in, last_out)
-}
-
-fn up_sampling(input: Vec<i16>, in_hz: u32, out_hz: u32) -> Vec<f32> {    
-  let edge = EDGE_SAMPLES - 1;
-  let rate = in_hz as f64 / out_hz as f64;
-  let out_sample_num = (INPUT_SAMPLES as f64) * ((out_hz as f64) / (in_hz as f64));
-
-  (0..out_sample_num as usize)
-    .map(|m| m as f64 * rate)    
-    .map(|x| {      
-      let num = x as usize;
-      let dec = x - num as f64;     
-
-      (num, dec)
-    })
-    .map(|(num, dec)| (num as usize, dec))
-    .map(|(idx, dec)| (idx - edge..idx + edge, dec))
-    .map(|(range, dec)| {
-      let start = -(edge as i32);      
-
-      range.zip(start..)
-        .map(|(idx, k)| input[idx] as f64 * sinc(k as f64 - dec))
-        .sum()
-    })
-    .map(|value: f64| value / i16::MAX as f64)    
-    .map(|value| value as f32)
-    .collect()    
-}
-
-fn sinc(x: f64) -> f64 {    
-  f64::sin(std::f64::consts::PI * x) / (std::f64::consts::PI * x)
-}
-
-fn clamp(x: i32) -> i16 {
-  if x > 0x7FFF {
-    0x7FFF
-  } else if x < -0x8000 {
-    -0x8000
-  } else {
-    x as i16
   }
 }
